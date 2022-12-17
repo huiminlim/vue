5851a34fe05a4273f1ae05057579900aa19b5bf3
huiminlim <e0036379@u.nus.edu>
2022-12-17 18:55:19 +0800

    Sending data from vue to render

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-Vue.createApp({}).mount("#app")
+Vue.createApp({
+    // Inserting data into application
+    // Evaluates the expression firstName to "John"
+    // Data that we don't wish to render should not be here (as it can be seen)
+    // Note: this data() function is a special function to return data from vue
+    data() {
+        return {
+            firstName: "John",
+            world: "Hello, world!"
+        }
+    },
+}).mount("#app")
